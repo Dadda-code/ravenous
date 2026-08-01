59e40dc7f97821f28953a773e4d551ce16b82e1f
Dadda-code <dadda.ancoravivo@gmail.com>
2026-08-01 16:21:53 +0200

    added the business component for easy tabs of businesses and updated the app.jsx

---
+++ src/components/Business.jsx
@@ -0,0 +1,26 @@
+import styles from '../modules/Business.module.css';
+
+function Business({ image, name, address, city, state, zipcode, category, rating, reviewCount }) {
+  return (
+    <div className={styles.business}>
+      <div className={styles['image-container']}>
+        <img src={image} alt={name} className={styles.img} />
+      </div>
+      <h2>{name}</h2>
+      <div className={styles['business-information']}>
+        <div className={styles['business-address']}>
+          <p>{address}</p>
+          <p>{city},</p>
+          <p>{state} {zipcode}</p>
+        </div>
+        <div className={styles['business-reviews']}>
+          <h3>{category}</h3>
+          <h3 className={styles.rating}>{rating}/5 ★</h3>
+          <p>{reviewCount} reviews</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Business;
